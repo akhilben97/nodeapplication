47cffbf8a6a1cb00dbebc8f62b4a39dc026c777c
AKHIL-B <akhil.benboss@gmail.com>
2024-07-20 15:35:57 +0530

    second

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var listener = function(request, response){
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.end('<h2 style="text-align: center;">Hello World</h2>');
+    
+
+}
+var server = http.createServer(listener);
+server.listen(3000);
+
+// Console will print the message
+
+console.log('Server running at http://127.0.0.1:3000/');
